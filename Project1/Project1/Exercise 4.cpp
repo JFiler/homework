ba85c58548d0782eb2c089161098be384e04f227
JFiler <jaylonfiler_jaylonfiler@aol.com>
2015-08-18 17:45:53 -0500

    JF
    My AiE assignments

---
+++ Project1/Project1/Exercise 4.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+int main()
+{
+	int a = 1;
+	int b = 2;
+	int temp = 3;
+
+	std::cout << "Please enter two numbers" << std::endl;
+	std::cin >> a, b;
+	std::cout << "value of a is : " << a << " " << "value of b is " << b << std::endl;
+
+	system("pause");
+}
